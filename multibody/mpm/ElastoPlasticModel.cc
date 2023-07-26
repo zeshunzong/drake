1c54c64d7bf9ae526ad8eca4df2de93e4648adf9
Zeshun Zong <zz1509@nyu.edu>
2023-07-26 13:14:58 -0700

    finish_stvk_dPdF_checkpoint

--- multibody/mpm/ElastoPlasticModel.cc
+++ multibody/mpm/ElastoPlasticModel.cc
@@ -13,11 +13,6 @@ ElastoPlasticModel<T>::ElastoPlasticModel(T E, T nu):
                                                 mu_(E/(2*(1+nu))) {
     DRAKE_ASSERT(E >= 0);
     DRAKE_ASSERT(nu > -1.0 && nu < 0.5);
-    T x = 1.5;
-    T y = 0.8;
-    T z = 0.2;
-    T w = 0.7;
-    std::cout << mathutils::diff_exp_over_diff(x, y, z)<< std::endl;
 }
 
 template class ElastoPlasticModel<double>;
