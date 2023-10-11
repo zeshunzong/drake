bc59ce0561af57a5fa9a2e8a1f6f90710ae0a471
Zenshun Zong <zz1509@nyu.edu>
2023-10-11 16:22:10 -0700

    Add documentation for poisson disk

--- multibody/mpm/internal/poisson_disk.h
+++ multibody/mpm/internal/poisson_disk.h
@@ -10,6 +10,13 @@ namespace multibody {
 namespace mpm {
 namespace internal {
 
+/**
+ * Returns a std::vector of point positions in 3D. Those randomly chosen points
+ * fill in the bounding box described by [xmin[0], xmax[0]]X[xmin[1],
+ * xmax[1]]X[xmin[2], xmax[2]], while maintaining a minimum distance of r
+ * between each other.
+ * See https://www.cs.ubc.ca/~rbridson/docs/bridson-siggraph07-poissondisk.pdf.
+ */
 std::vector<Vector3<double>> PoissonDiskSampling(
     double r, const std::array<double, 3>& x_min,
     const std::array<double, 3>& x_max);
