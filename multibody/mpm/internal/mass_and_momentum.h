676ae5c53acdfbaa76315a295e0f54ab2d5ca790
Zeshun Zong <37764481+zeshunzong@users.noreply.github.com>
2023-11-20 18:04:01 -0800

    Add hash for Vector3 and MassAndMomentum

---
+++ multibody/mpm/internal/mass_and_momentum.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "drake/common/eigen_types.h"
+
+namespace drake {
+namespace multibody {
+namespace mpm {
+namespace internal {
+
+// A struct storing the total mass, momentum, and angular momentum of the
+// deformable.
+// MPM transfer should preserve mass and both momentums.
+// @note Angular momentum is about the origin in world space.
+template <typename T>
+struct MassAndMomentum {
+  T total_mass = 0;
+  Vector3<T> total_momentum{};
+  Vector3<T> total_angular_momentum{};
+};
+
+}  // namespace internal
+}  // namespace mpm
+}  // namespace multibody
+}  // namespace drake
